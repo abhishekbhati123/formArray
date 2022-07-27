e492904ba1b06703153b2905ef8e774280a9b7a9
abhishekbhati123 <abhishekbhati1993@gmail.com>
2022-07-27 14:23:52 +0530

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,47 @@ import { Component, VERSION } from '@angular/core';
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+  title = 'angularcrudapp';
+  skillForm: FormGroup;
+  constructor(private fb: FormBuilder) {
+    this.skillForm = this.fb.group({
+      name: '',
+      skills: this.fb.array([]),
+    });
+  }
+
+  get skills(): FormArray {
+    //here we return the skills formsArray from the skillForms
+    return this.skillForm.get('skills') as FormArray;
+  }
+  newSkill(): FormGroup {
+    return this.fb.group({
+      skill: '',
+      exp: '',
+    });
+  }
+
+  //dynamically adding skills
+  addSkill() {
+    this.skills.push(this.newSkill());
+  }
+
+  //dynamically removing skills
+  removeSkil(i: number) {
+    this.skills.removeAt(i);
+  }
+  //submit
+  onSubmit() {
+    console.log(this.skillForm.value);
+  }
+}
+
+export class country {
+  id: string;
+  name: string;
+
+  constructor(id: string, name: string) {
+    this.id = id;
+    this.name = name;
+  }
 }
